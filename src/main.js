6d27743114abfc2af6efde9d5318983ef3d41811
yer <879908188@qq.com>
2020-01-16 20:34:11 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import MuseUIToast from 'muse-ui-toast/dist/muse-ui-toast';
+import 'typeface-roboto'
+
+Vue.use(MuseUI);
+Vue.use(MuseUIToast);
 
 Vue.config.productionTip = false
+document.addEventListener("deviceready",function () {
+    new Vue({
+        router,
+        store,
+        render: h => h(App)
+    }).$mount('#app')
+},false);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <mu-container class="button-wrapper">
+      <mu-button color="primary" @click="openCamera(true)">Primary</mu-button>
+      <mu-button color="secondary" @click="showTost">Secondary</mu-button>
+      <mu-button color="success">Success</mu-button>
+      <mu-button color="warning">Warning</mu-button>
+      <mu-button color="info">Info</mu-button>
+      <mu-button color="error">Error</mu-button>
+      <mu-button disabled>disabled</mu-button>
+    </mu-container>
+    <mu-icon value="home"></mu-icon>
+    <mu-alert color="success" v-if="successAlert">
+      this is success alert
+    </mu-alert>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "about",
+    data:function () {
+      return{
+        successAlert:false
+      }
+    },
+    methods:{
+      openCamera(){
+        navigator.camera.getPicture(onSuccess,onFail);
+        function onSuccess() {
+          alert("Camera cleanup success.")
+        }
+
+        function onFail(message) {
+          alert('Failed because: ' + message);
+        }
+      },
+      showTost(){
+        this.$toast.info("哈哈哈")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .button-wrapper {
+    text-align: center;
+  }
+  .button-wrapper .mu-button {
+    margin: 8px;
+  }
+</style>
